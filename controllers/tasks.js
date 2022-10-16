5d4fcc3df198d089e8be2c8b77db708369f7675f
FridahWatetu <fridahwatetumuthoni@gmail.com>
2022-10-17 00:47:15 +0300

    Task(2:5:00)

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,76 @@
+const Task = require('../models/Task')
+
 const getAllTasks = async (req, res) => {
-    res.send('Getting All the Tasks')
+    try{
+        const all_tasks = await Task.find().exec()
+        if(!all_tasks) return res.status(204).json({'message':"No Tasks Found"})
+        res.status(200).json(all_tasks)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({'message':`Name:${err?.errors?.name?.name} Message: ${err?.errors?.name?.message}`})
+    }
+    }
+
+const createNewTask =async (req, res) => {
+    /*if(!req?.body?.name || !req?.body?.completed){
+        return res.status(400).json({"message":"You must include the Name and Completed"})
+    }*/
+    try{
+        const task = await Task.create({name:req.body.name,completed:req.body.completed})
+        res.status(201).json(task)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({'message':`Name:${err?.errors?.name?.name} Message: ${err?.errors?.name?.message}`})
+    }
+    
 }
 
-const createNewTask = (req, res) => {
-    res.json(req.body)
+const updateTask =async (req, res) => {
+    const id =req.params.id
+    if(!id) return res.status(400).json({"message":"You must include an id"})
+    try{
+        const task = await Task.findOne({_id:id}).exec()
+        if(!task) return res.status(204).json({"message":`No Task matches ${id}`})
+        if(req?.body?.name) task.name = req.body.name
+        if(req?.body?.completed) task.completed = req.body.completed
+        const result = await task.save()
+        res.json(result)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({"message":"Incorrect id passed as the parameter"})
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send(`The id is in the parameter which is ${req.params.id}`)
+const deleteTask = async (req, res) => {
+    const id =req.params.id
+    try{
+        const get_task = await Task.findOne({_id:id}).exec()
+        if(!get_task) return res.status(404).json({"message":`No task with id ${id}`})
+        const result = await get_task.deleteOne({_id:id})
+        res.staus(200).json(result)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({"message":"Incorrect id passed as the parameter"})
+    }
+
 }
 
-const deleteTask = (req, res) => {
-    res.send('Deleting a task')
-}
+const getSingleTask = async (req, res) => {
+    const id =req.params.id
+    try{
+        const get_task = await Task.findOne({_id:id}).exec()
+        if(!get_task) return res.status(404).json({"message":`No task with id ${id}`})
+        res.status(200).json(get_task)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({"message":"Incorrect id passed as the parameter"})
+    }
 
-const getSingleTask = (req, res) => {
-    res.send('Getting a single task')
 }
 
 module.exports={getAllTasks,createNewTask,updateTask,deleteTask,getSingleTask}
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed:Boolean
+    name:{
+        type:String,
+        required:[true,"Must Provide a name"],
+        trim:true, //removing white space
+        maxlength:[40,'Name can not be more than 20 charaters']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
 
 //mongoose.model(Name, Schema)
